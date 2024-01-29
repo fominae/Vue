1ede55dea86f2eb53d51ec3d7026753fd4c1f078
Your Name <your_email@whatever.com>
2024-01-30 00:40:34 +0700

    Вывод вычисляемого свойства

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ let app = new Vue({
         brand: 'Vue Mastery',
         image: "./assets/vmSocks-green-onWhite.jpg",
         altText: "A pair of socks",
-        inStock: false,
+        inStock: true,
         details: ['80% cotton', '20% polyester', 'Gender-neutral'],
         variants: [
             {
